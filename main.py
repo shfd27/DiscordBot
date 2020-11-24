9ad1d3b400f3ff6dfe81e94fdc2cce0a7364cfe9
Kihyeon Kim <74966680+shfd27@users.noreply.github.com>
2020-11-24 21:32:32 +0900

    Ver 1.0.0

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import asyncio
+from discord.ext import commands
+from configs import configs
+import docs
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+#import configs
+bot_name=configs.bot_name
+TOKEN=configs.TOKEN
+PREFIX=configs.PREFIX
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+
+#import music module
+bot.load_extension("docs.music")
+
+
+
+@bot.event
+async def on_ready():
+    print(f"[Online] {bot.user}")
+    activity = discord.Game(f"{PREFIX}help | {bot_name}")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+
+
+
+#run bot
+bot.run(TOKEN)
