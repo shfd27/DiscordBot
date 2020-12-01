78c78b3d49cc2bf0381347d7a7ffbd643dd0e889
shfd27 <khkim4869@unist.ac.kr>
2020-12-01 23:58:20 +0900

    function play brings data of youtube music

--- docs/music.py
+++ docs/music.py
@@ -3,6 +3,15 @@ import asyncio
 from discord.ext import commands
 from discord.utils import get
 from configs import configs
+from docs import music_options
+
+
+def search_to_data(search):
+    import youtube_dl
+    ytdl = youtube_dl.YoutubeDL(music_options.ytdl_options)
+    data=ytdl.extract_info(search, download=False)
+    data=data["entries"][0]
+    return data
 
 
 class Music(commands.Cog, name="music"):
@@ -42,6 +51,30 @@ class Music(commands.Cog, name="music"):
             await ctx.message.channel.send("Leave from channel **"+str(bot_class_Member.voice.channel)+"**!")
         else:
             await ctx.message.channel.send("Bot is not in voice_channel!")
+
+    @commands.command()
+    async def play(self, ctx, *, search):
+        if ctx.message.author.voice!=None:
+            bot_voice_client=get(self.bot.voice_clients, guild=ctx.guild)
+            channel=ctx.message.author.voice.channel
+            if bot_voice_client==None:
+                await channel.connect()
+                await ctx.message.channel.send("Join to channel **"+str(ctx.message.author.voice.channel)+"**!")
+            else:
+                if self.bot.user not in channel.members:
+                    bot_class_Member=ctx.guild.get_member_named(str(self.bot.user))
+                    await ctx.message.channel.send("You are not in **"+str(bot_class_Member.voice.channel)+"**!")
+                    raise commands.CommandError("Author is in another voice_channel...")
+        else:
+            await ctx.message.channel.send("You are not in voice_channel!")
+            raise commands.CommandError("Author is not in voice_channel...")
+        
+        data=search_to_data(search)
+        
+
+
+
+
 
 
 def setup(bot):
---
+++ docs/music_options.py
@@ -0,0 +1,19 @@
+ytdl_options = {
+	"format": "bestaudio/best",
+	"audio_quality": 0,
+	"postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": "192"}, {"key": "FFmpegMetadata"}],
+	"outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+	"restrictfilenames": True,
+	"noplaylist": True,
+	"nocheckcertificate": True,
+	"ignoreerrors": False,
+	"logtostderr": False,
+	"quiet": True,
+	"no_warnings": True,
+	"default_search": "auto",
+	"source_address": "0.0.0.0"
+}
+
+ffmpeg_options = {"options": "-vn"}
+
+before_args = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
