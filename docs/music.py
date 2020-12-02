e9e91852de639c6936ead845dc09084a4f602667
shfd27 <khkim4869@unist.ac.kr>
2020-12-03 04:12:15 +0900

    embed is added

--- docs/music.py
+++ docs/music.py
@@ -24,6 +24,21 @@ def search_to_data(search):
     return data
 
 
+def play_embed(ctx):
+        data=music_data[ctx.guild.id][0]
+        YOUTUBE_ID=data['channel_id']
+        url=f"https://www.googleapis.com/youtube/v3/channels?part=snippet&id={YOUTUBE_ID}&key={API_KEY}"
+        api = urllib.request.urlopen(url).read()
+        api=eval(str(api.decode("utf-8")))
+        embed=discord.Embed(title=data['title'], url=data['webpage_url'], color=0xfff0a7) #, description="이것은 Embed입니다."
+        embed.set_author(name=data["uploader"], url=data['uploader_url'], icon_url=api["items"][0]['snippet']['thumbnails']['high']['url'])
+        embed.set_thumbnail(url=data['thumbnail'])
+        embed.add_field(name="Duration", value=str(datetime.timedelta(seconds=data['duration'])), inline=True)
+        embed.add_field(name="Requested by", value=ctx.author, inline=True)
+        embed.set_footer(text=ctx.author.voice.channel)
+        return ctx.channel.send(embed=embed)
+
+
 class Music(commands.Cog, name="music"):
     def __init__(self,bot):
         self.bot=bot
@@ -50,11 +65,12 @@ class Music(commands.Cog, name="music"):
                     await ctx.message.channel.send("Move to channel **"+str(ctx.message.author.voice.channel)+"**!")
         else:
             await ctx.message.channel.send("You are not in voice_channel!")
-        print(music_data)
 
 
     @commands.command()
     async def leave(self, ctx):
+        if music_data[ctx.guild.id]:
+            music_data[ctx.guild.id].clear()
         bot_voice_client=get(self.bot.voice_clients, guild=ctx.guild)
         if bot_voice_client!=None:
             bot_class_Member=ctx.guild.get_member_named(str(self.bot.user))
@@ -87,23 +103,24 @@ class Music(commands.Cog, name="music"):
             source=discord.FFmpegPCMAudio(data["url"], before_options=music_options.before_options, options=music_options.options)
             ctx.voice_client.play(source, after=lambda s: self.play_after(ctx))
             music_data[ctx.guild.id].append(data)
-            await ctx.message.channel.send(f"Playing **{data['title']}**!")
+            await play_embed(ctx)
         else:
             await ctx.message.channel.send(f"Queuing **{data['title']}**!")
             music_data[ctx.guild.id].append(data)
 
 
     def play_after(self,ctx):
-        music_data[ctx.guild.id].pop(0)
-        if music_data[ctx.guild.id]:
-            self.bot.loop.create_task(ctx.message.channel.send(f"Playing **{music_data[ctx.guild.id][0]['title']}**!"))
-            source=discord.FFmpegPCMAudio(music_data[ctx.guild.id][0]["url"], before_options=music_options.before_options, options=music_options.options)
-            ctx.voice_client.play(source, after=lambda s: self.play_after(ctx))
-        else:
-            music_data[ctx.guild.id].clear()
-            bot_class_Member=ctx.guild.get_member_named(str(self.bot.user))
-            self.bot.loop.create_task("Leave from "+str(bot_class_Member.voice.channel)+"**!")
-            self.bot.loop.create_task(ctx.voice_client.disconnect())
+        if self.bot.user in ctx.message.author.voice.channel.members:
+            music_data[ctx.guild.id].pop(0)
+            if music_data[ctx.guild.id]:
+                self.bot.loop.create_task(play_embed(ctx))
+                source=discord.FFmpegPCMAudio(music_data[ctx.guild.id][0]["url"], before_options=music_options.before_options, options=music_options.options)
+                ctx.voice_client.play(source, after=lambda s: self.play_after(ctx))
+            else:
+                music_data[ctx.guild.id].clear()
+                bot_class_Member=ctx.guild.get_member_named(str(self.bot.user))
+                self.bot.loop.create_task(ctx.message.channel.send("Leave from channel **"+str(bot_class_Member.voice.channel)+"**!"))
+                self.bot.loop.create_task(ctx.voice_client.disconnect())
 
 
 
