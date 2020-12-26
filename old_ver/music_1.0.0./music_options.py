7282d87526be56cd28ade1d0232a99384ce42ad5
Kihyeon Kim <74966680+shfd27@users.noreply.github.com>
2020-12-26 22:41:47 +0900

    Ver. 1.0.0. end

---
+++ old_ver/music_1.0.0./music_options.py
@@ -0,0 +1,21 @@
+ytdl_options = {
+	"format": "bestaudio/best",
+	"audio_quality": 0,
+	"postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": "192"}, {"key": "FFmpegMetadata"}],
+	"outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+	"restrictfilenames": True,
+	"noplaylist": True,
+	"nocheckcertificate": True,
+	"ignoreerrors": False,
+	"logtostderr": False,
+	"quiet": True,
+	"no_warnings": True,
+	"default_search": "auto",
+	"source_address": "0.0.0.0"
+}
+
+options="-vn"
+
+before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+
+API_KEY = '{YOUTUBE_API_KEY}'
