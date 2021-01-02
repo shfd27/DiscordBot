accb5a5235e2575e6cb071552448154bea7c2b15
shfd27 <khkim4869@unist.ac.kr>
2021-01-02 23:05:13 +0900

    funciton invite is available

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ bot = commands.Bot(command_prefix=PREFIX)
 bot.load_extension("docs.music")
 
 
+@bot.command()
+async def invite(ctx):
+    url="https://discord.com/api/oauth2/authorize?client_id="+str(bot.user.id)+"&permissions=0&scope=bot"
+    print(url)
+    embed=discord.Embed(title="Invite Link", url=url, color=0xfff0a7)
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    embed.add_field(name=f"Invite {bot_name}!", value=f"[「ㅤㅤㅤㅤ\nㅤInvite!ㅤ\nㅤㅤㅤㅤ」]({url})", inline=True)
+    embed.set_footer(text=str(ctx.message.channel))
+    await ctx.send(embed=embed)
+
+
 @bot.event
 async def on_ready():
     print(f"[Online] {bot.user}")
